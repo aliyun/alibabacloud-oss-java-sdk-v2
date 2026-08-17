7ee348dd716eb6682aa440826afb0d5216dfe87c
zhuxiaolong37 <zxl01071856@alibaba-inc.com>
2026-08-17 19:27:44 +0800

    extend InsightsImage configuration

--- src/main/java/com/aliyun/sdk/service/oss2/dataprocess/models/InsightsImageConfig.java
+++ src/main/java/com/aliyun/sdk/service/oss2/dataprocess/models/InsightsImageConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 /**
  * Insights.Image configuration within InsightsConfig.
+ * Contains Caption and Label sub-configs.
  */
 public final class InsightsImageConfig {
 
@@ -12,25 +13,34 @@ public final class InsightsImageConfig {
     @JacksonXmlProperty(localName = "Caption")
     private InsightsCaptionConfig caption;
 
+    @JsonProperty("Label")
+    @JacksonXmlProperty(localName = "Label")
+    private InsightsImageLabelConfig label;
+
     public InsightsImageConfig() {}
 
     private InsightsImageConfig(Builder builder) {
         this.caption = builder.caption;
+        this.label = builder.label;
     }
 
     public InsightsCaptionConfig caption() { return this.caption; }
+    public InsightsImageLabelConfig label() { return this.label; }
 
     public static Builder newBuilder() { return new Builder(); }
     public Builder toBuilder() { return new Builder(this); }
 
     public static class Builder {
         private InsightsCaptionConfig caption;
+        private InsightsImageLabelConfig label;
 
         public Builder caption(InsightsCaptionConfig value) { this.caption = value; return this; }
+        public Builder label(InsightsImageLabelConfig value) { this.label = value; return this; }
 
         private Builder() {}
         private Builder(InsightsImageConfig from) {
             this.caption = from.caption;
+            this.label = from.label;
         }
 
         public InsightsImageConfig build() { return new InsightsImageConfig(this); }
